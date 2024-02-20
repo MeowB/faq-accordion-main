70d23ac957337daa05d374a972a0ca7582982da7
MeowB <60074375+MeowB@users.noreply.github.com>
2024-02-20 20:28:49 +0100

    making progress

--- main.js
+++ main.js
@@ -1,52 +1,41 @@
 const acc = document.getElementsByClassName("accordion");
 let i;
 
+// accordion
 for (let i = 0; i < acc.length; i++) {
 	acc[i].addEventListener("click", function() {
-		const isActive = this.classList.contains("active");
-
-		hideInactivePanels();
+		const panel = this.nextElementSibling;
+		const panels = document.querySelectorAll(".panel")
+		const img = this.querySelector("img");
+		const imgs = document.querySelectorAll("button > img")
 		
 
-    	// Toggle the "active" class to highlight the clicked accordion
-	   	this.classList.toggle("active");
-		const img = this.querySelector("img");
-
-		const panel = this.nextElementSibling;
-
-        // If the accordion was already active, remove the "active" class and hide the panel
-		if (isActive) {
-            console.log("been there")
-			this.classList.remove("active")
-			panel.style.display = "none"
-			return
-        }
-
-        // Toggle between hiding and showing the active panel
+		// check if the panel clicked is already open to close it 
 		if (panel.style.display === "block") {
 			panel.style.display = "none";
 			img.src = "assets/images/icon-plus.svg"
 
-		} else {
+		} else { // if it's not already open, then open it
 			panel.style.display = "block";
 			img.src = "assets/images/icon-minus.svg"
 		}
 
+		// for each other panels, hide the content
+		for (let i = 0; i < panels.length; i++) {
+			const currentPanel = panels[i];
+			if (currentPanel !== panel) {
+				currentPanel.style.display = "none";
+			}
+		}
+
+		// check if the image match the one clicked on or else return to the plus image
+		for (let i = 0; i < imgs.length; i++) {
+			const temp = imgs[i];
+			if (temp != img) {
+				temp.src = "assets/images/icon-plus.svg"
+			}
+			
+		}
+
 	});
-}
-
-// Function to hide all inactive panels and reset all plus icons
-function hideInactivePanels() {
-	const panels = document.querySelectorAll('.panel');
-	const imgs = document.querySelectorAll(".accordion > img")
-
-    // Loop through each panel and hide it
-    panels.forEach(panel => {
-        panel.style.display = "none";
-    });
-	
-    // Loop through each image and reset its source to a plus icon
-	imgs.forEach(img => {
-		img.src = "assets/images/icon-plus.svg"
-	})
 }
